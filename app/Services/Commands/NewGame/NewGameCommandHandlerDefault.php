7d7a5bdb38951cc79a9ae25933a848856e15092b
danielescillia <daniele.scillia@tourradar.com>
2024-02-24 12:24:39 +0100

    feat: implemented logic of new Game creation

--- app/Services/Commands/NewGame/NewGameCommandHandlerDefault.php
+++ app/Services/Commands/NewGame/NewGameCommandHandlerDefault.php
@@ -2,11 +2,16 @@
 
 namespace App\Services\Commands\NewGame;
 
+use App\Models\Game;
+
 class NewGameCommandHandlerDefault implements NewGameCommandHandler
 {
 
     public function handle(): NewGameResult
     {
+        $game = new Game();
+        $game->saveOrFail();
 
+        return new NewGameResult($game->id);
     }
 }
